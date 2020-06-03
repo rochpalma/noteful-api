a8e6bf93bb158d313ab3242a357dcbc0bdda6519
Rochelle Palma <palmarochelleg@gmail.com>
2020-06-02 20:49:09 -0700

    changes for checkpoint

---
+++ seeds/seed.noteful.sql
@@ -0,0 +1,12 @@
+INSERT INTO folders (folder_name)
+VALUES
+  ('Important'),
+  ('Super'),
+  ('Spangley');
+
+INSERT INTO notes (note_name, content, folder_id)
+VALUES
+  ('Note One', 'Unang laman', 1),
+  ('Note Two', 'Pangalawang laman', 2),
+  ('Note Three', 'Pangatlong laman', 3)
+;
